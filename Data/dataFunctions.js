4d33322964edd6f32a617cebb4ee0b9676893794
a15freku <a15freku@student.his.se>
2018-03-22 11:10:51 +0100

    Working with canvas

---
+++ Data/dataFunctions.js
@@ -0,0 +1,53 @@
+var orgDataLen=data.length;
+
+// Creates more data from existing data
+function applyOffset(input) {
+  // Set how many graphs should be used
+  for(var i = 0; i < 200; i++) {
+    // Create more data
+    var src = input[i%orgDataLen];
+    var o = new Object();
+    o.name = src.name+i;
+    o.value = [];
+    input[orgDataLen+i] = o;
+    // Apply Offset
+    for(var j = 0; j < 251; j++) {
+
+      var max = 1.01;
+      var min = 0.99;
+      var r = Math.random() * (max - min) + min;
+      o.value[j] = src.value[j] * r;
+    }
+  }
+// Return offsetData
+return input;
+}
+
+// New Normalize data function
+function normalize(input){
+  for(var i = 0; i < input.length; i++) {
+
+    var length = input[i].value.length;
+    var min = input[i].value[0];
+    var max = min;
+
+    for(var j = 1; j < length; j++) {
+
+      var val = input[i].value[j];
+      min = Math.min(min, val);
+      max = Math.max(max, val);
+
+    }
+    var range = max - min;
+
+    var normArray = [];
+    for(var k = 0; k < length; k++) {
+
+      normArray.push((input[i].value[k] - min) / range);
+      
+    }
+    input[i].value = normArray;
+  }
+
+  return input;
+}
